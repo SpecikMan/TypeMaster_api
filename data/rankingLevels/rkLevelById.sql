61439b059657045bc44e2fd68e77062642b0d9de
Khang <h.v.khang68@gmail.com>
2021-12-06 16:29:59 +0700

    fix

--- data/rankingLevels/rkLevelById.sql
+++ data/rankingLevels/rkLevelById.sql
@@ -1,6 +1,6 @@
 select idRankingLevel,
 (SELECT FORMAT ([createDate], 'yyyy-MM-dd')) as [createDate],
-(SELECT FORMAT ([fromRankPeriod], 'yyyy-MM-dd')) as [fromRankPeriod]
+(SELECT FORMAT ([fromRankPeriod], 'yyyy-MM-dd')) as [fromRankPeriod],
 (SELECT FORMAT ([toRankPeriod], 'yyyy-MM-dd')) as [toRankPeriod],
 levelContent1,levelContent2,levelContent3,time1,time2,time3
  from RankingLevel
